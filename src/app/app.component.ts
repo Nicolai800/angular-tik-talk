5f13fca21cca01e9efee2d1f63ce2c23fd75323f
Nicolai800 <happy455420@gmail.com>
2024-12-10 14:34:29 +0100

    api; tofix style profile-card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { ProfileService } from './data/services/profile.service';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,11 @@ import { ProfileCardComponent } from './common-ui/profile-card/profile-card.comp
 })
 export class AppComponent {
   title = 'tik-talk';
+  profileService = inject(ProfileService);
+  profiles: Profile[] = [];
+  constructor() {
+    this.profileService
+      .getTestAccounts()
+      .subscribe((val) => (this.profiles = val));
+  }
 }
---
+++ src/app/data/interfaces/profile.interface.ts
@@ -0,0 +1,12 @@
+export interface Profile {
+  id: 123;
+  username: string;
+  avatarUrl: string | null;
+  subscribersAmount: number;
+  firstName: string;
+  lastName: string;
+  isActive: boolean;
+  stack: string[];
+  city: string;
+  description: string;
+}
