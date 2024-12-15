b2330bba9d29675738be1e5b25764b1a3348b271
Nicolai800 <happy455420@gmail.com>
2024-12-15 14:14:02 +0100

    auth, guard

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { TokenResponse } from './auth.interface';
+import { tap } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
@@ -7,10 +10,30 @@ import { inject, Injectable } from '@angular/core';
 export class AuthService {
   http = inject(HttpClient);
   baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
+  cookieServise = inject(CookieService);
+
+  token: string | null = null;
+  refreshToken: string | null = null;
+
+  get isAuth() {
+    if (!this.token) {
+      this.token = this.cookieServise.get('token');
+    }
+    return !!this.token;
+  }
+
   login(payload: { username: string; password: string }) {
     const fd = new FormData();
     fd.append('username', payload.username);
     fd.append('password', payload.password);
-    return this.http.post(`${this.baseApiUrl}token`, fd);
+    return this.http.post<TokenResponse>(`${this.baseApiUrl}token`, fd).pipe(
+      tap((val) => {
+        this.token = val.access_token;
+        this.refreshToken = val.refresh_token;
+
+        this.cookieServise.set('token', this.token);
+        this.cookieServise.set('refreshToken', this.refreshToken);
+      })
+    );
   }
 }
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -15,6 +16,7 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class LoginPageComponent {
   authServise = inject(AuthService);
+  router = inject(Router);
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
@@ -24,6 +26,7 @@ export class LoginPageComponent {
     if (this.form.valid) {
       //@ts-ignore  TODO:CREATE FORM TYPE
       this.authServise.login(this.form.value).subscribe((res) => {
+        this.router.navigate(['']);
         console.log(res);
       });
     }
