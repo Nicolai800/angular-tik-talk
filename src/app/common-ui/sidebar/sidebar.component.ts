2f8749ceb174ab533627e063eca11d759c2cd7c7
Nicolai800 <happy455420@gmail.com>
2024-12-16 16:05:40 +0100

    sidebar not finished

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgIconComponent, NgForOf],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Home Page',
+      icon: 'homePage',
+      link: '',
+    },
+    {
+      label: 'Chats',
+      icon: 'chats',
+      link: 'chats',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      link: 'search',
+    },
+  ];
 }
