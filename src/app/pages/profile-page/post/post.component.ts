dfdcdf5fff4b5e2e2250056247867a548dce7c34
Nicolai800 <happy455420@gmail.com>
2025-02-02 01:31:17 +0100

    post in process

--- src/app/pages/profile-page/post/post.component.ts
+++ src/app/pages/profile-page/post/post.component.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Post } from '../../../data/interfaces/post.interface';
+import { AvaratCircleComponent } from '../../../common-ui/avarat-circle/avarat-circle.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [AvaratCircleComponent, CommonModule],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
