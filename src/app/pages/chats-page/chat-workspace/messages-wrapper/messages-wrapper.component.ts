a03872320f01f95fba747cc4007a045088174e59
Nicolai800 <happy455420@gmail.com>
2025-02-10 19:50:02 +0100

    chats components

---
+++ src/app/pages/chats-page/chat-workspace/messages-wrapper/messages-wrapper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messages-wrapper',
+  imports: [],
+  templateUrl: './messages-wrapper.component.html',
+  styleUrl: './messages-wrapper.component.scss'
+})
+export class MessagesWrapperComponent {
+
+}
